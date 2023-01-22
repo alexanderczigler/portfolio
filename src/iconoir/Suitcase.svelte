d9fac4dea612772a670df892a7d67ea6cc33883d
Alexander Czigler <dev@ilix.se>
2023-01-22 22:20:39 +0100

    fix: currentColor

--- src/iconoir/Suitcase.svelte
+++ src/iconoir/Suitcase.svelte
@@ -7,7 +7,7 @@
 	xmlns="http://www.w3.org/2000/svg"
 	><path
 		d="M8 7H4a2 2 0 00-2 2v10a2 2 0 002 2h16a2 2 0 002-2V9a2 2 0 00-2-2h-4M8 7V3.6a.6.6 0 01.6-.6h6.8a.6.6 0 01.6.6V7M8 7h8"
-		stroke="#000000"
+		stroke="currentColor"
 		stroke-width="1.5"
 	/></svg
 >
