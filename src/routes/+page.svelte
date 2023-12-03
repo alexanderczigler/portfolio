30e2bfa0f789511941cfcd339808c25fe269d8ef
Alexander Czigler <git@ilix.se>
2023-12-03 21:00:30 +0100

    feat: trying something

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,23 +9,42 @@
 </svelte:head>
 
 <h1>Alexander Czigler</h1>
+<hr />
 
-<span>
-	<a href="https://ilix.se/alexanderczigler-cv.pdf" target="_blank" rel="noreferrer">
-		<Pages />
-	</a>
+<p>Links</p>
 
-	<a href="https://github.com/alexanderczigler" target="_blank" rel="noreferrer">
-		<GitHub />
-	</a>
+<div class="list">
+	<ul>
+		<li>
+			<a href="https://ilix.se/alexanderczigler-cv.pdf" target="_blank" rel="noreferrer">
+				<Pages /> CV
+			</a>
+		</li>
+		<li>
+			<a href="https://github.com/alexanderczigler" target="_blank" rel="noreferrer">
+				<GitHub /> GitHub
+			</a>
+		</li>
+		<li>
+			<a href="https://www.linkedin.com/in/alexanderczigler" target="_blank" rel="noreferrer">
+				<LinkedIn /> LinkedIn
+			</a>
+		</li>
+	</ul>
+</div>
 
-	<a href="https://www.linkedin.com/in/alexanderczigler" target="_blank" rel="noreferrer">
-		<LinkedIn />
-	</a>
-</span>
+<hr />
+
+<p>&copy;2023 Alexander Czigler</p>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300');
+
+	body {
+		font-family: 'Fira Code', monospace;
+		background-color: #fff;
+		color: #000;
+	}
 
 	a,
 	a:visited {
@@ -39,8 +58,22 @@
 
 	h1 {
 		color: #0b4371;
-		font-family: 'Fira Code';
 		font-weight: 100;
 		font-size: 1.5rem;
+	}
+
+	.list ul {
+		list-style: none;
+		padding: 0;
+	}
+
+	.list li {
+		margin-bottom: 0.5rem;
+	}
+
+	hr {
+		border: 0;
+		height: 1px;
+		background-color: #ccc;
 	}
 </style>
